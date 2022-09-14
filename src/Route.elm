02f446b2eba36f3176c37cff7b1ceef7f52ec3b0
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2022-09-14 14:55:57 +0200

    Add Post detail view (initial layout)

--- src/Route.elm
+++ src/Route.elm
@@ -1,6 +1,6 @@
 module Route exposing (Language(..), Page(..), Route, routeParser)
 
-import Url.Parser exposing ((</>), Parser, map, oneOf, s, top)
+import Url.Parser exposing ((</>), Parser, map, oneOf, s, string, top)
 
 
 type alias Route =
@@ -13,6 +13,7 @@ type Page
     = Home
     | Profile
     | Blog
+    | Post String
 
 
 type Language
@@ -25,8 +26,19 @@ routeParser =
     oneOf
         [ map { page = Profile, language = Default } (s "profile")
         , map { page = Blog, language = Default } (s "blog")
+        , map (\pageId -> { page = Post pageId, language = Default }) (s "blog" </> string)
         , map { page = Home, language = Japanese } (s "jp")
         , map { page = Profile, language = Japanese } (s "jp" </> s "profile")
         , map { page = Blog, language = Japanese } (s "jp" </> s "blog")
+        , map (\pageId -> { page = Post pageId, language = Japanese }) (s "jp" </> s "blog" </> string)
         , map { page = Home, language = Default } top
         ]
+
+
+
+-- createJapaneseRoute : Page -> Route
+-- createJapaneseRoute page =
+--     { page = page, language = Japanese }
+-- post : Parser (Route -> a) a
+-- post =
+--     map Post
